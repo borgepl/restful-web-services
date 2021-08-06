07f198c0f8d2700d1ee78a5f4561d95c292b26b8
borgepl <pdeborger@gmail.com>
2021-08-06 15:54:51 +0200

    Updated to be able to deploy to Weblogic + added Spring Actuator for monitoring

--- src/main/java/com/deborger/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/deborger/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -2,17 +2,25 @@ package com.deborger.rest.webservices.restfulwebservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 import java.util.Locale;
 
 @SpringBootApplication
-public class RestfulWebServicesApplication {
+public class RestfulWebServicesApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(RestfulWebServicesApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(RestfulWebServicesApplication.class);
     }
 
     @Bean
