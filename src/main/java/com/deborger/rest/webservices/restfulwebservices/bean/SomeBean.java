88f1c5b3df2464a9166cf91dcf8eda2f04ef63aa
borgepl <pdeborger@gmail.com>
2021-08-09 09:46:01 +0200

    Filtering example

---
+++ src/main/java/com/deborger/rest/webservices/restfulwebservices/bean/SomeBean.java
@@ -0,0 +1,59 @@
+package com.deborger.rest.webservices.restfulwebservices.bean;
+
+import com.fasterxml.jackson.annotation.JsonFilter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+//Static Filter out "field1" and "field2" - using @JsonIgnoreProperties
+//@JsonIgnoreProperties(value = {"field1","field2"})
+
+//Dynamic filtering with filters defined in the method
+@JsonFilter("SomeBeanFilter")
+public class SomeBean {
+
+    private String field1;
+    private String field2;
+
+    // sensitive data in field3 - not returning - using @JsonIgnore - Static fitlering
+    //@JsonIgnore
+    private String field3;
+
+    public SomeBean(String field1, String field2, String field3) {
+        this.field1 = field1;
+        this.field2 = field2;
+        this.field3 = field3;
+    }
+
+    public String getField1() {
+        return field1;
+    }
+
+    public void setField1(String field1) {
+        this.field1 = field1;
+    }
+
+    public String getField2() {
+        return field2;
+    }
+
+    public void setField2(String field2) {
+        this.field2 = field2;
+    }
+
+    public String getField3() {
+        return field3;
+    }
+
+    public void setField3(String field3) {
+        this.field3 = field3;
+    }
+
+    @Override
+    public String toString() {
+        return "SomeBean{" +
+                "field1='" + field1 + '\'' +
+                ", field2='" + field2 + '\'' +
+                ", field3='" + field3 + '\'' +
+                '}';
+    }
+}
